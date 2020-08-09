e776cf619a99990e5084a638ed9d0cf3459eac07
jairovera17 <jairovera17@gmail.com>
2020-08-08 19:15:31 -0500

    Subscribe value, error, complete

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,16 @@ const obs$ = new Observable<string>(subs => {
     subs.next("Hola");
     subs.next("Mundo");
 
+    const a = undefined;
+    a.name = "Jairo";
+
     subs.complete();
     //Los siguientes valores no seán subscritos
     subs.next("Aloooooo");
 });
 
-obs$.subscribe(console.log)
+obs$.subscribe(
+    value => console.log(`El valor es ${value}`),
+    error => console.warn(`El error es ${error}`),
+    () => console.log("Completado")
+)
