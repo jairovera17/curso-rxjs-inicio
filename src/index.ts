b7a977766cafb9b62acc0d24bfb6084a2c400d3a
jairovera17 <jairovera17@gmail.com>
2020-08-08 19:06:58 -0500

    Primer observable

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
+import { Observable } from "rxjs";
 
+const obs$ = new Observable<string>(subs => {
+    subs.next("Hola");
+    subs.next("Mundo");
 
-console.log('Hola Mundo!');
+    subs.complete();
+    //Los siguientes valores no seán subscritos
+    subs.next("Aloooooo");
+});
 
-
-
-
-
-
-
+obs$.subscribe(console.log)
